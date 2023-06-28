bbd52e8235b96d041901cb09a84d5a9f63de33c3
ebellon-arch <115672769+ebellon-arch@users.noreply.github.com>
2023-06-28 15:30:50 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,6 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_cho
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output in the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized) 
 
 import snowflake.connector 
